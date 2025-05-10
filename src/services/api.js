5e62d913f7d2d94bc1f45f4755819ac9b25ade23
jtiagotoledo <jtiagotoledo@gmail.com>
2025-05-10 08:09:50 -0300

    criado crud completo para professores padrão rest

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://assistente-professor.duckdns.org:3000';
+const API_URL = 'https://assistente-professor.duckdns.org:3000';
 
 const api = axios.create({
   baseURL: API_URL,
---
+++ src/services/professores.js
@@ -0,0 +1,56 @@
+import api from './api';
+
+// GET /professores
+export const buscarTodosProfessores = async () => {
+  try {
+    const resposta = await api.get('/professores');
+    return resposta.data;
+  } catch (erro) {
+    console.error('Erro ao buscar todos os professores', erro);
+    throw erro;
+  }
+};
+
+// GET /professores/:id
+export const buscarProfessorPorId = async (id) => {
+  try {
+    const resposta = await api.get(`/professores/${id}`);
+    return resposta.data;
+  } catch (erro) {
+    console.error(`Erro ao buscar professor com id ${id}`, erro);
+    throw erro;
+  }
+};
+
+// POST /professores
+export const criarProfessor = async (dados) => {
+  try {
+    const resposta = await api.post('/professores', dados);
+    return resposta.data;
+  } catch (erro) {
+    console.error('Erro ao criar professor', erro);
+    throw erro;
+  }
+};
+
+// PUT /professores/:id
+export const atualizarProfessor = async (id, dados) => {
+  try {
+    const resposta = await api.put(`/professores/${id}`, dados);
+    return resposta.data;
+  } catch (erro) {
+    console.error(`Erro ao atualizar professor com id ${id}`, erro);
+    throw erro;
+  }
+};
+
+// DELETE /professores/:id
+export const deletarProfessor = async (id) => {
+  try {
+    const resposta = await api.delete(`/professores/${id}`);
+    return resposta.data;
+  } catch (erro) {
+    console.error(`Erro ao deletar professor com id ${id}`, erro);
+    throw erro;
+  }
+};
